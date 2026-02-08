3424294e7ca29339d35e286b3085c049c0a03a29
K RAMYA <reddykramya1@gmail.com>
2026-02-08 18:50:02 +0530

    Update TRAINING.py

--- TRAINING.py
+++ TRAINING.py
@@ -7,28 +7,22 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
-
-# =========================
 # CONFIG
-# =========================
+
 DATA_DIR = r"C:\Users\kuruv\PycharmProjects\hello world\ASL_Data"
 CLASS_NAMES = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 NUM_CLASSES = 26
 EPOCHS = 30
 BATCH_SIZE = 32
 
-# =========================
 # NORMALIZATION FUNCTION
-# =========================
 def normalize_landmarks(landmarks):
     landmarks = np.array(landmarks).reshape(-1, 3)
     wrist = landmarks[0]              # landmark 0 = wrist
     landmarks = landmarks - wrist
     return landmarks.flatten()
 
-# =========================
 # LOAD DATA
-# =========================
 data = []
 labels = []
 
@@ -47,24 +41,19 @@ labels = np.array(labels)
 
 print("Total samples:", data.shape[0])
 print("Feature size:", data.shape[1])
-
-# =========================
 # LABEL ENCODING
-# =========================
 le = LabelEncoder()
 labels_encoded = le.fit_transform(labels)
 labels_onehot = to_categorical(labels_encoded, NUM_CLASSES)
 
-# =========================
 # TRAIN TEST SPLIT
-# =========================
+
 X_train, X_test, y_train, y_test = train_test_split(
     data, labels_onehot, test_size=0.2, random_state=42
 )
 
-# =========================
+
 # MODEL (MLP)
-# =========================
 model = Sequential([
     Dense(256, activation='relu', input_shape=(data.shape[1],)),
     Dropout(0.4),
@@ -79,9 +68,7 @@ model.compile(
     metrics=['accuracy']
 )
 
-# =========================
 # TRAIN
-# =========================
 history = model.fit(
     X_train, y_train,
     validation_data=(X_test, y_test),
@@ -89,17 +76,14 @@ history = model.fit(
     batch_size=BATCH_SIZE
 )
 
-# =========================
 # SAVE MODEL
-# =========================
 model.save("asl_landmark_dl_model.h5")
 
-# =========================
 # PLOT ACCURACY
-# =========================
 plt.plot(history.history['accuracy'], label='Train Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
 plt.legend()
 plt.show()
+
